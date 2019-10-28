a1cfc30ce1e0276fd577009adbe3da3fb1273fb0
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-10-28 19:20:01 +0100

    Copy code from tabula

---
+++ R/utilities.R
@@ -0,0 +1,38 @@
+# HELPERS
+
+#' Helpers
+#'
+#' \code{compact} removes elements from a list or vector.
+#' \code{detect} xxx.
+#' \code{count} xxx.
+#' \code{extract} extracts string form another string based on a pattern.
+#'
+#' %o% allows for function composition.
+#' %||% allows to define a default value.
+#' @param x,y An object.
+#' @param f,g A \code{\link{function}}. In \code{compact}, \code{detect}
+#'  and \code{count} \code{f} must be a logical predicate.
+#' @param pattern A \code{\link{character}} string containing a regular
+#'  expression.
+#' @references
+#'  Wickham, H. (2014). \emph{Advanced R}. London: Chapman & Hall. The R Series.
+#' @keywords internal
+#' @noRd
+`%||%` <- function(x, y) {
+  if (!is.null(x) || length(x) != 0) x else y
+}
+`%o%` <- function(f, g) {
+  function(...) f(g(...))
+}
+compact <- function(f, x) {
+  Filter(Negate(f), x)
+}
+detect <- function(f, x) {
+  vapply(x, f, logical(1))
+}
+count <- function(f, x) {
+  sum(detect(f, x))
+}
+extract <- function(x, pattern) {
+  regmatches(x, regexpr(pattern, x))
+}
