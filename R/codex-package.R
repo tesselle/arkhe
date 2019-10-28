9fffed82d27184ed6cae98209b5279c352410668
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-10-28 15:56:28 +0100

    Update DESCRITPION

---
+++ R/codex-package.R
@@ -0,0 +1,33 @@
+#' @details
+#' \codexr{ll}{
+#'  \strong{Package:} \tab codex \cr
+#'  \strong{Type:} \tab Package \cr
+#'  \strong{Version:} \tab 0.0.0.9000 \cr
+#'  \strong{License:} \tab GPL-3 \cr
+#'  \strong{DOI:} \tab \href{https://doi.org/xxx}{xxx}
+#' }
+#' @author
+#' \strong{Full list of authors and contributors} (alphabetic order)
+#'
+#' \codexr{ll}{
+#'  Nicolas Frerebeau \tab \emph{Université Bordeaux Montaigne, France} \cr
+#' }
+#'
+#' \strong{Package maintainer}
+#'
+#' Nicolas Frerebeau\cr
+#' \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
+#'
+#' IRAMAT-CRP2A (UMR 5060)\cr
+#' Maison de l'Archéologie\cr
+#' Université Bordeaux Montaigne\cr
+#' F-33607 Pessac cedex\cr
+#' France
+#' @name codex-package
+#' @aliases codex
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom methods new setClass setClassUnion setGeneric setMethod
+NULL
