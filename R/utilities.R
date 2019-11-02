471fa7f43f90fede87134ac9db782161f66dd95e
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-11-02 13:22:32 +0100

    Document utilities

--- R/utilities.R
+++ R/utilities.R
@@ -3,15 +3,17 @@
 #' Helpers
 #'
 #' \code{compact} removes elements from a list or vector.
-#' \code{detect} xxx.
-#' \code{count} xxx.
-#' \code{extract} extracts string form another string based on a pattern.
+#' \code{detect} finds values in a list or vector according to a given
+#' predicate.
+#' \code{count} counts values in a list or vector according to a given
+#' predicate.
+#' \code{extract} extracts a string form another string based on a pattern.
 #'
 #' %o% allows for function composition.
 #' %||% allows to define a default value.
 #' @param x,y An object.
 #' @param f,g A \code{\link{function}}. In \code{compact}, \code{detect}
-#'  and \code{count} \code{f} must be a logical predicate.
+#'  and \code{count} \code{f} must be a \code{\link{logical}} predicate.
 #' @param pattern A \code{\link{character}} string containing a regular
 #'  expression.
 #' @references
--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -5,12 +5,19 @@ test_that("Compact", {
   expect_length(compact(is.null, list("A", NULL, "B")), 2)
 })
 test_that("Detect", {
-  expect_type(detect(is.na, c(1, 2, NA, 4, 5, NA)), "logical")
-  expect_equal(sum(detect(is.na, c(1, 2, NA, 4, 5, NA))), 2)
+  k <- detect(is.na, c(1, 2, NA, 4, 5, NA))
+  expect_type(k, "logical")
+  expect_equal(sum(k), 2)
 })
 test_that("Count", {
-  expect_type(count(is.na, c(1, 2, NA, 4, 5, NA)), "integer")
-  expect_equal(count(is.na, c(1, 2, NA, 4, 5, NA)), 2)
+  k <- count(is.na, c(1, 2, NA, 4, 5, NA))
+  expect_type(k, "integer")
+  expect_equal(k, 2)
+})
+test_that("Extract", {
+  k <- extract(c("abc123", "def456", "ghi"), "[1-9]{3}")
+  expect_type(k, "character")
+  expect_length(k, 2)
 })
 test_that("Function composition", {
   expect_type((sum %o% range)(1:5), "integer")
