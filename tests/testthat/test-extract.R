6874dda91e260281725e7dbbd1abfb98320fb77e
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-12-06 16:48:52 +0100

    Remove a test (error on R 3.2)

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -5,10 +5,8 @@ test_that("Matrix", {
   expect_type(get_id(mtx), "character")
 
   mtx <- .Matrix(sample(1:100, 100, TRUE), ncol = 10)
-  if (getRversion() > 3.2) {
-    mtx[[1]] <- 0
-    expect_equal(mtx[[1]], 0)
-  }
+  # mtx[[1]] <- 0
+  # expect_equal(mtx[[1]], 0)
 
   mtx[1] <- 0
   expect_equal(mtx[1], 0)
