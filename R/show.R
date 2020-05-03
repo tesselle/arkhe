42fcbc2154535386b772944b6eadaef45aed443e
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2020-05-03 15:34:56 +0200

    Update show method

--- R/show.R
+++ R/show.R
@@ -8,12 +8,6 @@ setMethod(
   definition = function(object) {
     cat(sprintf("<%s: %s>\n", class(object),
                 paste0(object@size, collapse = " x ")))
-  }
-)
-setMethod(
-  f = "print",
-  signature = "DataMatrix",
-  definition = function(x) {
-    as_matrix(x)
+    print(as_matrix(object))
   }
 )
